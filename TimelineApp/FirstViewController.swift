a91a0fbe494416c1e11fa53cfcd4f85a7be3920f
junappleseed <junappleseed@gmail.com>
2016-01-10 00:11:56 +0900

    Initial Commit

---
+++ TimelineApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TimelineApp
+//
+//  Created by Jun on 2016/01/10.
+//  Copyright © 2016年 junappleseed. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
